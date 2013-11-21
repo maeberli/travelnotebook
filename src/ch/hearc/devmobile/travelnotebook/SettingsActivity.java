32ce8a9b8581566394fefee451ec52ae08aaddba
David Kuhner <dav.kuhn@gmail.com>
2013-11-21 15:46:04 +0100

    Add missing fils

---
+++ src/ch/hearc/devmobile/travelnotebook/SettingsActivity.java
@@ -0,0 +1,39 @@
+package ch.hearc.devmobile.travelnotebook;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+		
+		Button btnSave = (Button) findViewById(R.id.save_settings);
+		btnSave.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				SettingsActivity.this.saveSettings();
+				SettingsActivity.this.finish();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+	
+	private void saveSettings() {
+		// save settings here
+	}
+
+}
